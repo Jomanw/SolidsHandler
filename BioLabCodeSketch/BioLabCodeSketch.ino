9e82bff9986c9cc72ddbd74ca68a253466958c80
Jordan Wick <wickj@mit.edu>
2018-05-04 02:27:48 -0400

    Updated skeleton code.

--- BioLabCodeSketch/BioLabCodeSketch.ino
+++ BioLabCodeSketch/BioLabCodeSketch.ino
@@ -1,22 +1,40 @@
+/*
+ * Runs the Solids Handler bot
+ * 
+ * Connections:
+ * First solid dispenser servo control pin: 7
+ * Second solid dispenser servo control pin: 8
+ * Dump Tray servo control pin: 9
+ * 
+ * Uses a PD control strategy to control flow rate
+ */
 
 
+// TODO: Import relevant libraries for reading scale
 #include <Servo.h>
 int firstServoPin = 7;
 Servo firstServo;
 int secondServoPin = 8;
 Servo secondServo;
 int dumpTrayServo = 9;
-int currentServer;
+Servo currentServo;
+int servoUsed = 1;
 String readString;
+
+// CHANGE THIS PARAMETER TO CONTROL FLOW RATE AND TOLERANCE
+int P = 10;
+int D = 10;
+float epsilon = 0.03;
 
 int readScale() {
   // TODO: Make this actually call a function that returns the reading from the scale. Also correct the return type.
   return 100;
 }
 
-
+/*
+ * Switches the servo being used from the first to the second, and vice versa
+ */
 void switchServo() {
-  // Switches the servo being used from the first to the second
   if (currentServo == firstServo) {
     currentServo = secondServo;
   } else {
@@ -24,6 +42,54 @@ void switchServo() {
   }
 }
 
+/*
+ * Sets the flow rate of a given servo. TODO: use different functions to map input values linearly to flow rates. 
+ */
+ void setFlowRate(Servo servo, double flowRate) {
+    // Set the servo to some position, probably dependent on some f(flowRate) --> actual servo position, from 0 to 180 degrees. 
+ }
+
+
+/*
+ * Dispenses a given amount of solids onto the scale, as accurately as it can
+ */
+void dispenseSolid(int servo, int amount) {
+  
+  // Will calculate an approximation of the derivative by using the old scale value, keeping track of the previous timesteps
+  int oldReading = readScale();
+  int bias = oldReading();
+  int newReading;
+  int difference;
+  int oldTime = millis();
+  int newTime;
+  int timeDifference = 0;
+  int error;
+  int currentFlowRate = 0;
+  float derivative;
+  // Goes through a loop of checking the scale reading, opening or closing the things as a 
+  while (true) {
+     newReading = readScale();
+     difference = newReading - oldReading;
+     error = amount - newReading + bias;
+
+     if (error < epsilon) {
+      break;
+     }
+     newTime = millis();
+     
+     timeDifference = newTime - oldTime;
+
+     // Approximation of the derivative of mass / time
+     derivative = difference / (float) timeDifference;
+
+     currentFlowRate += error * P + derivative * D;
+     setFlowRate(currentServo, currentFlowRate);
+     
+
+     oldReading = newReading;
+     oldTime = newTime;
+  }
+}
 
 
 void setup() {
@@ -43,23 +109,7 @@ void setup() {
 
 
 
-void dispenseSolid(int servo, int amount) {
-  // Dispenses solids, of a given amount.
 
-  // Will calculate an approximation of the derivative by using the old scale value
-  int oldReading = readScale();
-  int newReading = readScale();
-  int difference;
-  int oldTime = time.millis();
-  int newTime = time.millis();
-  int timeDifference = 0;
-  // Goes through a loop of checking the scale reading, opening or closing the things as a 
-  while (true) {
-     newReading = readScale();
-     difference = newREading - oldReading;
-     newTime= 
-  }
-}
 
 
 void loop() {
